f3d2c50d283e30b33d044fc74ed919f685832e9f
EcstaticFly <suyashpandey607@gmail.com>
2025-01-22 08:05:38 +0530

    added registerUser functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,27 +1,61 @@
+import { generateToken } from "../configs/utils.js";
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
 
+export const loginUser = async (req, res) => {
+  try {
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
-
-export const loginUser = async(req,res)=>{
-    try{
-
-    }catch(e){
-        console.log(e.message);
+export const registerUser = async (req, res) => {
+  try {
+    const { email, password, fullName } = req.body;
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: "Please fill in all fields" });
     }
-}
-
-
-export const registerUser = async(req,res)=>{
-    try{
-
-    }catch(e){
-        console.log(e.message);
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
     }
-}
-
-export const logoutUser = async(req,res)=>{
-    try{
-
-    }catch(e){
-        console.log(e.message);
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
     }
-}
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      fullName,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      return res.status(201).json({
+        _id: newUser._id,
+        email: newUser.email,
+        fullName: newUser.fullName,
+        profilePic: newUser.profilePic,
+        message: "User created successfully",
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const logoutUser = async (req, res) => {
+  try {
+  } catch (e) {
+    console.log(e.message);
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import dotenv, { configDotenv } from "dotenv";
+import cors from "cors";
+import cookieParser from 'cookie-parser'
 import connectDB from "../server/configs/mongoDB.js";
 
 dotenv.config({
@@ -10,9 +12,27 @@ dotenv.config({
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is now running on port ${PORT}`);
-    connectDB();
+  console.log(`Server is now running on port ${PORT}`);
+  connectDB();
 });
