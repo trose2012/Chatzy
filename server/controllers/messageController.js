52bf36bf3b26ad82d0ef15d47f307e4eba54c024
EcstaticFly <suyashpandey607@gmail.com>
2025-01-23 08:08:11 +0530

    added some message controller functions

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,22 +1,58 @@
+import Message from "../models/message.js";
 import User from "../models/user.js";
+import { v2 as cloudinary } from "cloudinary";
 
-export const getUsers = async(req,res)=>{
-    try {
-        const loggedInUserId = req.user._id;
-        const users = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
-        res.status(200).json(users);
-    }catch(e){
-        console.log(e.message);
-        res.status(500).json({ message: "Failed to fetch users" });
+export const getUsers = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const users = await User.find({ _id: { $ne: loggedInUserId } }).select(
+      "-password"
+    );
+    res.status(200).json(users);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: "Failed to fetch users" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = req.user._id;
+    const messages = await Message.find({
+      $or: [
+        { senderId: currentUserId, receiverId: id },
+        { senderId: id, receiverId: currentUserId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: "Failed to fetch messages" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+    let imageUrl;
+    if (image) {
+      const result = await cloudinary.uploader.upload(image);
+      imageUrl = result.secure_url;
     }
-}
-
-export const getMessages = async(req,res)=>{
-    try {
-        const {id} = req.params;
-
-    }catch(e){
-        console.log(e.message);
-        res.status(500).json({ message: "Failed to fetch messages" });
-    }
-}
+    const message = await Message.create({
+      text,
+      senderId,
+      receiverId,
+      image: imageUrl,
+    });
+    await message.save();
+    res.status(201).json(message);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: "Failed to send message" });
+  }
+};
--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyjwt } from "../middlewares/checkAuth.js";
-import { getMessages, getUsers } from "../controllers/messageController.js";
+import { getMessages, getUsers, sendMessage } from "../controllers/messageController.js";
 const router = express.Router();
 
 router.get('/users',verifyjwt,getUsers);
 router.get('/:id',verifyjwt,getMessages);
+router.post('/send/:id',verifyjwt,sendMessage);
 
 
 export default router;
